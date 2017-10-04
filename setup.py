ea482ef2500b6ff7946791a5b09c0390b9ac9286
David Taieb <david_taieb@us.ibm.com>
2017-10-04 19:03:50 -0400

    added requirements to setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ setup(name='pixiedust_health',
 	     version='0.1',
 	     description='Sample Notebook PixieApp for Watson Health',
 	     url='https://github.com/ibm-watson-data-lab/pixiedust_health',
-	     install_requires=['pixiedust'],
+	     install_requires=['pixiedust', 'sklearn', 'scipy'],
 	     author='David Taieb',
 	     author_email='david_taieb@us.ibm.com',
 	     license='Apache 2.0',
 	     packages=find_packages(),
 	     include_package_data=True,
 	     zip_safe=False
-	)
+	    )
